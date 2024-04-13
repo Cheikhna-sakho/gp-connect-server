7760f58aca85a086cb1c356f2ff3d7631f16d90e
cheikhna-sakho <cheikh.bsakho@gmail.com>
2024-04-14 00:18:26 +0200

    feat: import advertisements module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { AdvertisementsModule } from './advertisements/advertisements.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, AdvertisementsModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
