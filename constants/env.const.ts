efe2a9bd2bf136ccbcc3c2db9a676b8d1942eb56
cheikhna-sakho <cheikh.bsakho@gmail.com>
2024-04-23 15:51:34 +0200

    add env config constant

---
+++ constants/env.const.ts
@@ -0,0 +1,8 @@
+export const envConfig = {
+  PORT: process.env.PORT,
+  environment: process.env.NODE_ENV,
+};
+export const environment = {
+  development: 'development',
+  production: 'production',
+};
