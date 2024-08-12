12cecd3e6b9a82bb55ce3a8b234a06547f9a4df7
cheikhna-sakho <cheikh.bsakho@gmail.com>
2024-08-12 02:43:24 +0200

    fix: import module missing

--- src/medias/medias.module.ts
+++ src/medias/medias.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MediasService } from './medias.service';
 import { MediasController } from './medias.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   providers: [MediasService],
   controllers: [MediasController],
   exports: [MediasService],
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CloudinaryModule],
 })
 export class MediasModule {}
--- src/packages/packages.module.ts
+++ src/packages/packages.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PackagesController } from './packages.controller';
 import { PackagesService } from './packages.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { MediasModule } from 'src/medias/medias.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MediasModule],
   controllers: [PackagesController],
   providers: [PackagesService],
 })
